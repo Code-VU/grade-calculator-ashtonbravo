b25c97f916f874f2410e967520b708e8fbc3f66d
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-02 21:54:52 -0500

    test 8

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -5,20 +5,20 @@ def calculateGrade():
 
     score = input("Enter Score: ")
     try:
-    s= float(score)
-    if s<= 1.0:
-        if s>=0.9 :
-            print ('A')
-        elif s>=0.8:
-            print=("B")
-        elif s>= 0.7:
-            print=("C")
-        elif s>=0.6:
-            print=("D")
-        elif s<= 0.6:
-            print=("F")
-    else:
-        print= ("Bad score")
+        s= float(score)
+        if s<= 1.0:
+            if s>=0.9 :
+                print ('A')
+            elif s>=0.8:
+                print=("B")
+            elif s>= 0.7:
+                print=("C")
+            elif s>=0.6:
+                print=("D")
+            elif s<= 0.6:
+                print=("F")
+        else:
+            print= ("Bad score")
  
         
 
