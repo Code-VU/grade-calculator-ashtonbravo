4c5fd4f46510dd658a1cda3b3ad652d1652b0a2d
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-02 23:49:46 -0500

    test again

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,30 +2,26 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
+Print('Calculating Grade')
 
-    score =input("Enter Score: ")
-    s= float(score)
-    x= "Bad Score"
-    if score<= 1.0:
+try: 
+    score = float(input( 'Enter Score: '))
 
-        if s>=0.9 :
-            print ('A')
+    if score > 1:
+        print('Bad score')
+    elif score >= 0.9:
+        print('A')
+    elif score >= 0.8:
+        print('B')
+    elif score >= 0.7:
+        print('C')
+    elif score>= 0.6:
+        print ('D')
+    elif score >= 0:
+        print('F')
+except:
+    print('Bad score')
 
-        elif s>=0.8:
-            print=("B")
-
-        elif s>= 0.7:
-            print=("C")
-
-        elif s>=0.6:
-            print=("D")
-
-        elif s<= 0.6:
-            print=("F")
-    else:
-        print= ("x")
- 
-        
 
     # end assignment
 
